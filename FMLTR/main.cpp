4c62fa98812759055dfbaf2f5b7a4be7d33849c3
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-02-03 04:44:44 +0300

    edit README.md
    edit README.md - bdl

--- FMLTR/main.cpp
+++ FMLTR/main.cpp
@@ -3,73 +3,56 @@
 
 int main()
 {
-    // Create the window in full screen mode
-    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Bouncing DVD logo", sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Bouncing DVD logo", 
+        sf::Style::Fullscreen);
+    window.setFramerateLimit(60);
 
-    // Set the square position and size
-    sf::RectangleShape square;
-    square.setSize(sf::Vector2f(50, 50));
-    square.setPosition(window.getSize().x / 2 - square.getSize().x / 2, window.getSize().y / 2 - square.getSize().y / 2);
+    sf::Texture texture;
+    texture.loadFromFile("dvd.png");
+    sf::Sprite sprite(texture);
+    sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
 
-    // Set the fill color of the square
-    square.setFillColor(sf::Color::Red);
+    sf::Vector2f direction(5.0f, 5.0f);
+    sf::Color color(255, 0, 0);
 
-    // Initial velocity for the square
-    sf::Vector2f velocity(100.0f, 100.0f);
-
-    // Create a clock to measure the elapsed time
-    sf::Clock clock;
-
-    // Start the game loop
     while (window.isOpen())
     {
-        // Handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+            {
+                if (window.hasFocus())
+                    window.close();
             }
         }
 
-        // Measure the elapsed time
-        float elapsedTime = clock.restart().asSeconds();
-
-        // Update the square position
-        square.move(velocity * elapsedTime);
-
-        // Check if the square has hit the edge of the window
-        if (square.getPosition().x <= 0 || square.getPosition().x + square.getSize().x >= window.getSize().x)
+        sprite.move(direction);
+        if (sprite.getPosition().x + sprite.getLocalBounds().width / 2 >= window.getSize().x || sprite.getPosition().x - sprite.getLocalBounds().width / 2 <= 0)
         {
-            velocity.x = -velocity.x;
-            square.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            direction.x = -direction.x;
+            color.r = rand() % 255;
+            color.g = rand() % 255;
+            color.b = rand() % 255;
+            sprite.setColor(color);
         }
-        if (square.getPosition().y <= 0 || square.getPosition().y + square.getSize().y >= window.getSize().y)
+        if (sprite.getPosition().y + sprite.getLocalBounds().height / 2 >= window.getSize().y || sprite.getPosition().y - sprite.getLocalBounds().height / 2 <= 0)
         {
-            velocity.y = -velocity.y;
-            square.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
-        }
-
-        // Check if the square has fallen into a corner of the window
-        if ((square.getPosition().x <= 0 && square.getPosition().y <= 0) ||
-            (square.getPosition().x + square.getSize().x >= window.getSize().x && square.getPosition().y <= 0) ||
-            (square.getPosition().x <= 0 && square.getPosition().y + square.getSize().y >= window.getSize().y) ||
-            (square.getPosition().x + square.getSize().x >= window.getSize().x && square.getPosition().y + square.getSize().y >= window.getSize().y))
-        {
-            window.close();
+            direction.y = -direction.y;
+            color.r = rand() % 255;
+            color.g = rand() % 255;
+            color.b = rand() % 255;
+            sprite.setColor(color);
         }
 
-        // Clear the window
         window.clear();
-
-        // Draw the square
-        window.draw(square);
-
-        // Display the changes
+        window.draw(sprite);
         window.display();
     }
 
     std::cout << "Program terminated." << std::endl;
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FMLTR
+# Bouncing DVD logo on c++ (SFML-2.5.1)
+The "bouncing DVD logo" refers to an animation that was commonly used as the startup or loading screen for many DVD players. 
+The animation typically consists of a rotating DVD logo that bounces or moves back and forth on the screen. 
+The purpose of the animation was to indicate that the DVD player was starting up or loading the disc, 
+and to provide a visual cue to the user that the player was working as intended.
